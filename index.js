b0c50a07aebda71bfb70ad86915667ca3e1206f6
Hadj Said Bouras <hadjsaidbouras@hotmail.com>
2023-07-30 06:15:23 +0100

    Done with the assignment

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Code your solution in this file!
+
+const drivers = ['any', 'oaos', 'dsfsd', 'dsofjh', 'wdfsf']
+
+const returnFirstTwoDrivers = function (par){
+    const newArray = par.slice(0, 2);
+    return newArray;
+}
+
+const returnLastTwoDrivers = function (any) {
+    const nnew = any.slice(any.length -2, any.length + 1)
+    return nnew;
+}
+
+const a = returnFirstTwoDrivers;
+const b = returnLastTwoDrivers;
+
+const selectingDrivers = [a, b]
+
+console.log(selectingDrivers)
+
+const createFareMultiplier = function (multiplier) {
+    const fareMultiplier = function (fare) {
+      return fare * multiplier;
+    };
+    return fareMultiplier;
+  };
+
+  const fareDoubler = function (fare){
+    return createFareMultiplier(2)(fare)
+  }
+
+  const fareTripler = function (fare) {
+    return createFareMultiplier(3)(fare)
+  }
+
+function selectDifferentDrivers(array, fun) {
+  return fun(array)
+}
